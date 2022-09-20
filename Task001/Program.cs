4871714087c5338bfc29c20b6939fe67fa30b2e2
Listratova Tatyana <listratova1@gmail.com>
2022-09-20 14:51:13 +0300

    added Task001

---
+++ Task001/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task001
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Пользователь вводит М чисел, узнать сколько чисел больше нуля
+
+            Console.WriteLine(" Введите общее количество чисел: ");
+
+            int count = int.Parse(Console.ReadLine());
+
+            int[] myArray = new int[count];
+
+            for (int i = 0; i < myArray.Length; i++)
+            {
+                Console.WriteLine($"Введите число под номером {i + 1}");
+
+                myArray[i] = int.Parse(Console.ReadLine());
+            }
+
+            int GetPositiveDigits(int[] array)
+            {
+                int result = 0;
+
+                for (int i = 0; i < array.Length; i++)
+                {
+                    if (array[i] > 0)
+                    {
+                        result++;
+                    }
+                }
+                return result;
+             }
+
+            int resultPositive = GetPositiveDigits(myArray);
+
+            Console.WriteLine($"В введенном массиве {resultPositive } положительных чисел");
+
+        }
+    }
+}
